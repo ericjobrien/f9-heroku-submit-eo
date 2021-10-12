8fc3678cbf811b7e7ca3dec6ee534e50fd8c0dd2
Eric O'Brien <obrienjeric@gmail.com>
2021-10-12 14:46:23 -0400

    adding rollbar

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const path = require('path');
+const Rollbar = require('rollbar');
 
+const rollbar = new Rollbar( {
+    accessToken: '83efd38be67d4eea85cf8c558d327f76',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+});
 const app = express();
 
+//middleware to use rollbar
+app.use(rollbar.errorHandler());
+//middleware to use json
+app.use(express.json());
+
+
+//middleware to access the html file
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, "../public/index.html"))});
+
+//middleware to access the css file
 app.get('/stylesheet.css', (req, res) => { res.sendFile(path.join(__dirname, "../public/stylesheet.css"))});
 
 
